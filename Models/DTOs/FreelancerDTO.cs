71c700700a313242a76c9725aee84c6063128b46
KAJOiq <abdalrahmanahmed298@gmail.com>
2024-12-18 12:35:19 +0300

    fix bug QualificationName in FreelancerResponseDTO

--- Models/DTOs/FreelancerDTO.cs
+++ Models/DTOs/FreelancerDTO.cs
@@ -6,7 +6,8 @@ namespace AonFreelancing.Models.DTOs
    
     public class FreelancerResponseDTO : UserResponseDTO
     {
-        public List<SkillOutputDTO>Skills { get; set; }
+        public string? QualificationName { get; }
+
         FreelancerResponseDTO(Freelancer freelancer, string imageBaseUrl)
         {
             Id = freelancer.Id;
@@ -16,6 +17,7 @@ namespace AonFreelancing.Models.DTOs
             About = freelancer.About;
             PhoneNumber = freelancer.PhoneNumber;
             UserType = Constants.USER_TYPE_FREELANCER;
+            QualificationName = freelancer.QualificationName;
             IsPhoneNumberVerified = freelancer.PhoneNumberConfirmed;
             //Skills = freelancer.Skills.Select(s => SkillOutputDTO.FromSkill(s)).ToList();
             //Role = new RoleResponseDTO { Name = Constants.USER_TYPE_FREELANCER };
